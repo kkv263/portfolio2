2d0bbd7367e4f0513757b15a8668280179280961
kkv263 <kkv263@utexas.edu>
2017-10-17 14:56:14 -0500

    add react-hot-loader🔥

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {AppContainer} from 'react-hot-loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+      const App = require('./App').default; // eslint-disable-line global-require
+      ReactDOM.render(
+          <AppContainer>
+              <App />
+          </AppContainer>,
+          document.getElementById('root')
+      );
+  });
+}
